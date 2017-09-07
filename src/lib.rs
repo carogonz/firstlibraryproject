17dc450f258081eb58aa13a7dc1148538653787c
Carolina Gonzales <caro.gonz91@gmail.com>
2017-09-06 23:10:09 -0500

    first library project

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+mod clinic{
+    fn procedure () {
+    }
+
+    mod dentist{
+        fn procedure () {
+
+
+     }
+ }
+	
+
+    mod patient{
+       fn treatment() {
+	}
+     }
+
+}
